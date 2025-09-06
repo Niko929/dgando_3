f28090192357c34bf804342f8b67ddf3af0b1aa5
MSI <ekaterinanikolskaa2@gmail.com>
2025-09-06 15:41:25 +0700

    home 30.2

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,11 +2,21 @@ from rest_framework import generics, permissions
 from rest_framework import serializers
 from .models import Course, Lesson
 
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
-        fields = '__all__'
+        fields = [
+            'id',
+            'title',
+            'description',
+            'preview_image',
+            'price',
+            'lessons_count',
+            'lessons',  # Добавляем поле с уроками
+            'created_at',
+            'updated_at'
+        ]
+        read_only_fields = ['created_at', 'updated_at']
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -15,7 +25,9 @@ class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = ['id', 'title', 'description', 'lessons_count']
 
     def get_lessons_count(self, obj):
         return obj.lessons.count()
+
+
